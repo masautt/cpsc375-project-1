c62f6da1f6dc27815d8a0cf618868d45b5dd1a45
masautt <marek.sautter@gmail.com>
2020-04-20 20:16:34 -0700

    Up to 1d

--- main.r
+++ main.r
@@ -1,2 +1,18 @@
 library(tidyverse)
 
+# 1.a) Reading the Data using read_csv()
+rawConfirmed <- read_csv("https://raw.githubusercontent.com/masautt/cpsc375-project-1/master/confirmed.csv")
+rawDeaths <- read_csv("https://raw.githubusercontent.com/masautt/cpsc375-project-1/master/deaths.csv")
+rawHospitalBeds <- read_csv("https://raw.githubusercontent.com/masautt/cpsc375-project-1/master/hospitalbeds.csv")
+rawDemographics <- read_csv("https://raw.githubusercontent.com/masautt/cpsc375-project-1/master/demographics.csv")
+
+# 1b) Tidying Tables if Needed
+gatheredConfirmed <- rawConfirmed %>% gather(key="date", value="numConfirmed", -`Province/State`, -`Country/Region`, -`Lat`, -`Long`)
+gatheredDeaths <- rawDeaths %>% gather(key="date", value="numDeaths", -`Province/State`, -`Country/Region`, -`Lat`, -`Long`)
+
+# 1c) Filter hospital beds to recent year
+filteredHospitalBeds <- rawHospitalBeds %>% filter(Year == 2015)
+
+# 1d) Aggregate Confirmed and Deaths to country level
+groupedConfirmed <- gatheredConfirmed
+groupedDeaths
