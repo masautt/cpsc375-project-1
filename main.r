79fbbadfab2e5505cdbf706dae81b5b811b39d69
masautt <marek.sautter@gmail.com>
2020-04-21 19:19:29 -0700

    Added groupedDeaths

--- main.r
+++ main.r
@@ -7,12 +7,12 @@ rawHospitalBeds <- read_csv("https://raw.githubusercontent.com/masautt/cpsc375-p
 rawDemographics <- read_csv("https://raw.githubusercontent.com/masautt/cpsc375-project-1/master/demographics.csv")
 
 # 1b) Tidying Tables if Needed
-gatheredConfirmed <- rawConfirmed %>% gather(key="date", value="numConfirmed", -`Province/State`, -`Country/Region`, -`Lat`, -`Long`)
-gatheredDeaths <- rawDeaths %>% gather(key="date", value="numDeaths", -`Province/State`, -`Country/Region`, -`Lat`, -`Long`)
+gatheredConfirmed <- rawConfirmed %>% gather(key="Date", value="NumConfirmed", -`Province/State`, -`Country/Region`, -`Lat`, -`Long`)
+gatheredDeaths <- rawDeaths %>% gather(key="Date", value="NumDeaths", -`Province/State`, -`Country/Region`, -`Lat`, -`Long`)
 
 # 1c) Filter hospital beds to recent year
 filteredHospitalBeds <- rawHospitalBeds %>% filter(Year == 2015)
 
 # 1d) Aggregate Confirmed and Deaths to country level
-groupedConfirmed <- gatheredConfirmed
-groupedDeaths
+groupedConfirmed <- gatheredConfirmed %>% group_by(`Country/Region`, `Date`) %>% summarise(NumConfirmed = sum(NumConfirmed))
+groupedDeaths <- gatheredDeaths %>% group_by(`Country/Region`, `Date`) %>% summarise(NumDeaths = sum(NumDeaths))
